d28f96f746641a09b59b5acdea177d2f5754ea03
SAIRAMKADAKUNTLA <sairam.kadakuntla30@gmail.com>
2021-06-01 17:20:43 +0530

    Added:Length of the line

---
+++ Line____Camparision/Line____Camparision/Linecamparision.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Line____Camparision
+{
+    public class Linecamparision
+    {
+        int x1, x2, y1, y2;
+        public void Length(int x1, int x2, int y1, int y2)
+        {
+            this.x1 = x1;
+            this.x2 = x2;
+            this.y1 = y1;
+            this.y2 = y2;
+            double Length = +(Math.Sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2));
+            Console.WriteLine("Length of line =" + Length);
+
+        }
+    }
+}
---
+++ Line____Camparision/Line____Camparision/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Line____Camparision
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to line camparision");
+            Linecamparision linecamparision = new Linecamparision();
+            linecamparision.Length(25,26,20,25);
+        }
+    }
+}
